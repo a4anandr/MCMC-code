e6714e34558a2fbeda07505e77fa4f32aae19b3d
Anand Radhakrishnan <a4anandr@gmail.com>
2018-08-15 20:43:24 -0400

    Put an additional check on the condition number of the norm matrix to ensure decent results in each trial

--- Swiss banknotes/kernelAv_cov_fast.m
+++ Swiss banknotes/kernelAv_cov_fast.m
@@ -11,10 +11,11 @@ function [thetaav, b ] = kernelAv_cov_fast(theta,z)
 
 tic; 
 [n, d]  = size(theta);
-train_i = randsample(n, 100);    % 0.0025*n
-train_n = length(train_i);
-theta_train = theta(train_i,:);
+train_n = 200;
+check   = 40;           % Initializing at 10^40, assigned after observing from a number of trials, the condition number is usually ~ 10^20 for good results
+iteration = 1;          % To count the number of iterations it takes to get training samples that give a lower condition number.
 theta_mean = mean(theta,1); 
+% theta_mean = [-0.7117    0.7969    0.9974    3.0062];   % Just hard coding the mean
 M = zeros(train_n,train_n);
 b = zeros(train_n,d);
 
@@ -22,13 +23,26 @@ b = zeros(train_n,d);
 % Gaussian kernel and its derivatives
 lambda  = 1e-7;   % 1e-6 or small value is optimal
 epsilon = 2;  % So far 1.5 - 2 is the best
-
 tic
-for i = 1 : n 
-    for j =  1 : train_n
-        norm_matrix(i,j) = norm(theta(i,:) - theta_train(j,:))^2;
+v = version('-release');
+while(check > 25)
+    train_i     = randsample(n, train_n);    
+    theta_train = theta(train_i,:);
+    if v == '2018a'
+        for j  = 1 : train_n
+            norm_matrix(:,j) = vecnorm((theta - repmat(theta_train(j,:),n,1)),2,2).^2;
+        end
+    else
+        for i = 1 : n
+            for j =  1 : train_n
+                norm_matrix(i,j) = norm(theta(i,:) - theta_train(j,:))^2;
+            end         
+        end
     end
+    check = log10(cond(norm_matrix(train_i,:)));           % To ensure that highly similar samples are not chosen in the training set. This will cause high condition number of the matrix and in turn give poor results.
+    iteration = iteration + 1;
 end
+
 K  = exp(-norm_matrix / (4 * epsilon));
 for j = 1 : train_n
     for k = 1 : d
