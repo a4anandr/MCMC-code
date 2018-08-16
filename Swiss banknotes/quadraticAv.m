5d4c0b872dc7a9289ece03c97e61243823cd71fb
Anand Radhakrishnan <a4anandr@gmail.com>
2018-08-15 20:59:34 -0400

    Minor changes in all the files

--- Swiss banknotes/quadraticAv.m
+++ Swiss banknotes/quadraticAv.m
@@ -1,4 +1,4 @@
-function [thetaZv, a] = quadraticAZv(theta, z)
+function [thetaZv, a] = quadraticAv(theta, z)
 % quadraticAZv: calculation of ZV-RMHMC estimates using a quadratic
 % polynomial.
 
@@ -27,11 +27,9 @@ end
 [b, a] = deal(zeros(d*(d+3)/2, d));
 
 for m = 1:1:n    
-    grad_psi(:,:,m) = [eye(4); diag(theta(m,:)); theta(m,2) theta(m,1) 0 0; theta(m,3) 0 theta(m,1) 0; theta(m,4) 0 0 theta(m,1);0 theta(m,3) theta(m,2) 0; 0 theta(m,4) 0 theta(m,2); 0 0 theta(m,4) theta(m,3)];
+    grad_psi(:,:,m) = [eye(d); diag(theta(m,:)); theta(m,2) theta(m,1) 0 0; theta(m,3) 0 theta(m,1) 0; theta(m,4) 0 0 theta(m,1);0 theta(m,3) theta(m,2) 0; 0 theta(m,4) 0 theta(m,2); 0 0 theta(m,4) theta(m,3)];
     M = M + (1/n) * grad_psi(:,:,m) * grad_psi(:,:,m)';     % could be (1/(n-1))
 end 
-
-cond(M)
  
 for i = 1:d
    b(:, i)  = (1/n)* psi' *(theta(:,i) - theta_mean(i));   
